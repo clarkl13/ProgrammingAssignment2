5b07af887b4c2e4499e4f7c6f0311479e06d8336
clarkl13 <ljf5006@gmail.com>
2017-10-25 21:20:38 -0400

    Problem Set 2 Wk 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## creates a object which creates 4 functions and returns the functions into a list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inv <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solv) inv <<- solv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
+## Returns a matrix that is the inverse of the returned makecachematrix object
+## if the same returned makecachematrix value is passed into the cacheSolve function 
+## returns "getting cached data" text along with actual cached data
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  inv <- x$getinv()
+  if(!is.null(inv)) 
+  {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
